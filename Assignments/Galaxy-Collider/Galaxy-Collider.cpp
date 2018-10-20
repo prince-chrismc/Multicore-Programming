4cd74b773e13549618754e0af9fad2b97d940880
Chris McArthur <prince.chrismc@gmail.com>
2018-10-19 20:15:10 -0400

    trying pipeline with threaded glfw
    5 fps

--- Assignments/Galaxy-Collider/Galaxy-Collider.cpp
+++ Assignments/Galaxy-Collider/Galaxy-Collider.cpp
@@ -37,7 +37,7 @@ SOFTWARE.
 #include "Quadrant.h"
 
 #include "tbb/parallel_for_each.h"
-#include "tbb/parallel_invoke.h"
+#include <tbb/pipeline.h>
 
 void key_callback( GLFWwindow* window, int key, int scancode, int action, int mode );
 
@@ -95,10 +95,7 @@ int main( int argc, char** argv )
    auto shaderProgram = Shader::Linked::GetInstance();
 
    Galaxy galaxy_one( ObjectColors::BLUE, 8.0f, -5.0f, 1.5f, 15000 );
-   //Galaxy galaxy_two( ObjectColors::RED, 5.0f, -4.0f, 0.25f, 2000 );
    Galaxy galaxy_small( ObjectColors::GREEN, -5.5f, 8.1f, 2.125f, 25000 );
-
-   //root << galaxy_one;
 
    const auto calcForOnStarRange = []( Blackhole blackhole ) {
       return [ blackhole = blackhole ]( std::pair<const glm::vec2, Particle>& star )
@@ -139,24 +136,35 @@ int main( int argc, char** argv )
 
       shaderProgram->SetUniformMat4( "view_matrix", camera->GetViewMatrix() );
       shaderProgram->SetUniformMat4( "projection_matrix", window->GetProjectionMatrix() );
-      GlfwWindow::FreeWindow();
-      // Draw Loop
+      window->FreeWindow();
 
-      tbb::parallel_invoke( [ galaxy_small, galaxy_one ] {
-         GlfwWindow::GetInstance()->SelectWindow();
-         galaxy_small.Draw();
-         galaxy_one.Draw();
-         GlfwWindow::FreeWindow();
-                            },
-                            [ & ] {
-                               tbb::parallel_for_each( galaxy_small.m_Stars.begin(), galaxy_small.m_Stars.end(),
-                                                       calcForOnStarRange( galaxy_small.m_Blackhole )
-                               );
+      size_t galaxyCounter = 2;
 
-                               tbb::parallel_for_each( galaxy_one.m_Stars.begin(), galaxy_one.m_Stars.end(),
-                                                       calcForOnStarRange( galaxy_one.m_Blackhole )
-                               );
-                            } );
+      tbb::parallel_pipeline( 2, tbb::make_filter<void, Galaxy*>( tbb::filter::mode::serial_in_order,
+                              [ &galaxyCounter, &galaxy_one, &galaxy_small ]( tbb::flow_control& fc )->Galaxy*
+                              {
+                                 switch( --galaxyCounter )
+                                 {
+                                 case 1:
+                                    return &galaxy_one;
+                                 case 0:
+                                    return &galaxy_small;
+                                 default:
+                                    fc.stop();
+                                    break;
+                                 }
+                                 return nullptr;
+                              } ) &
+                              tbb::make_filter<Galaxy*, Galaxy*>( tbb::filter::mode::serial_in_order, []( Galaxy* galaxy ) {
+                                 GlfwWindow::GetInstance()->SelectWindow();
+                                 galaxy->Draw();
+                                 GlfwWindow::GetInstance()->FreeWindow();
+                                 return galaxy;
+                              } ) &
+                                 tbb::make_filter<Galaxy*, void>( tbb::filter::mode::parallel, [ &calcForOnStarRange ]( Galaxy* galaxy ) {
+                                    tbb::parallel_for_each( galaxy->m_Stars.begin(), galaxy->m_Stars.end(), calcForOnStarRange( galaxy->m_Blackhole ) );
+                              } )
+                           );
 
       window->SelectWindow();
       window->NextBuffer();
